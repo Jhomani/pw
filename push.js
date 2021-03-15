4fcf01b9bd2df9a8475dbcd37674d09458dced18
jhomani <jhomani@kali.jhomani>
2021-03-14 23:20:29 -0500

    fixing finish

--- push.js
+++ push.js
@@ -6,12 +6,11 @@ const vapikeys = {
     privateKey: 'izDK7ofgkSG8xzcid1LX-jKHdfh9YQ7nvUA0YftPpGo'
   }
   
-push.setVapidDetails("jhomani@gmail.com", vapikeys.publicKey, vapikeys.privateKey);
+push.setVapidDetails("mailto:test@code.co.uk", vapikeys.publicKey, vapikeys.privateKey);
 
 let sub = {
-    
+  "endpoint": "",
+"keys":{"auth":"ZEGmiQ5YMs1q7E3TJHdcDg","p256dh":"BOCFC5c6s5pNnrXbXF_SXs4imM61r9MrP97RIgTvkYVWKUaAgpNw9kuG9xMMPMx-n2CyyQ1K0ky3MCTm3g66_Hk"}
 }
 
 push.sendNotification(sub, "test message");
-
-console.log(keys)
